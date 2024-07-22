31070c236db4ffd0b3c9d6b384fa52e0ef57f077
devinengr <79027895+devinengr@users.noreply.github.com>
2024-07-22 13:56:12 -0400

    add info popup when coin is clicked

---
+++ Assets/Scripts/GameObjectClickHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class UIShowOnGameObjectClick : MonoBehaviour {
+
+    public XRScreenSpaceController controller;
+    public XRRayInteractor raycastInteractor;
+    public UnityEvent actionsOnClick;
+
+    void Start() {
+        InputActionProperty tapAction = controller.tapStartPositionAction;
+        InputActionProperty dragAction = controller.dragCurrentPositionAction;
+        if (Application.isEditor) {
+            dragAction.action.canceled += ctx => RaycastCallback(ctx);
+        } else {
+            tapAction.action.started += ctx => RaycastCallback(ctx);
+        }
+    }
+
+    void RaycastCallback(InputAction.CallbackContext ctx) {
+        if (raycastInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit3D)) {
+            GameObject obj = hit3D.transform.gameObject;
+            if (obj == gameObject) {
+                actionsOnClick.Invoke();
+            }
+        }
+    }
+
+}
